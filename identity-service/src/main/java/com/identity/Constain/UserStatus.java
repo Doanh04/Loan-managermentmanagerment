b377fc82008b717e2d89c4fcdcd9a2bcdcf65677
Phạm Đức Doanh <112089369+Doanh04@users.noreply.github.com>
2026-06-10 21:59:34 +0700

    Delete identity-service directory

--- identity-service/src/main/java/com/identity/Constain/UserStatus.java
+++
@@ -1,9 +0,0 @@
-package com.identity.Constain;
-
-public enum UserStatus {
-    ACTIVE, //Đã kích hoạt
-    IN_ACTIVE, // Chưa kích hoạt
-    BANNED, // Khóa tài khoản
-    LOCKED,// Tạm khóa,
-    WAITING_ACTIVE
-}
