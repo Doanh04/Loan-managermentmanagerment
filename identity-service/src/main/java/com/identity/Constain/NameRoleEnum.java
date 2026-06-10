78acdc2c1d1b9262c39f3e36510f8ec7549587fe
Phạm Đức Doanh <112089369+Doanh04@users.noreply.github.com>
2026-06-10 21:57:59 +0700

    Delete identity-service directory

--- identity-service/src/main/java/com/identity/Constain/NameRoleEnum.java
+++
@@ -1,9 +0,0 @@
-package com.identity.Constain;
-
-public enum NameRoleEnum {
-    ROLE_ADMIN,
-    ROLE_LOAN_UNDERWRITER,
-    ROLE_LOAN_MANAGER,
-    ROLE_ACCOUNTANT,
-    ROLE_CUSTOMER
-}
--- identity-service/src/main/java/com/identity/Constain/RolesEnum.java
+++
@@ -1,9 +0,0 @@
-package com.identity.Constain;
-
-public enum RolesEnum {
-    ADMIN,//Quản lý
-    LOAN_UNDERWRITER, //Nhân viên tín dụng
-    LOAN_MANAGER, //Giám đốc chi nhánh,
-    ACCOUNTANT,// Kế toán
-    CUSTOMER,//Người dùngdùng
-}
