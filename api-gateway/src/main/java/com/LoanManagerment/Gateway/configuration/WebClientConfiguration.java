c6a6fd2cc7e390871c87496dcc4ee6870e341963
Doanh Pham <dgrunt04@gmail.com>
2026-06-14 00:53:52 +0700

    feat(WebClientConfiguration): create AuthenticationFilter and WebclientConfiguration

--- api-gateway/src/main/java/com/LoanManagerment/Gateway/configuration/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/LoanManagerment/Gateway/configuration/WebClientConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
 
 @Configuration
 public class WebClientConfiguration {
@@ -16,7 +21,18 @@ public class WebClientConfiguration {
                 .build();
     }
 
+    @Bean
+    CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
 
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+    }
     @Bean
     IdentityClient identityClient(WebClient webClient){
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
